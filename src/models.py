8a0d00f60fc4575e3db011cffd1c8678ab56432f
R Aishwarya Prabhu <aishwaryaprabhur7@gmail.com>
2025-04-18 19:08:18 +0530

    [feath]:VGG & ResNet model architecture

---
+++ src/models.py
@@ -0,0 +1,17 @@
+import torchvision.models as models
+import torch.nn as nn
+
+def get_model(model_name, num_classes):
+    if model_name == "vgg":
+        model = models.vgg16(pretrained=True)
+        model.classifier[6] = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(4096, num_classes)
+        )
+    elif model_name == "resnet":
+        model = models.resnet18(pretrained=True)
+        model.fc = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(model.fc.in_features, num_classes)
+        )
+    return model
